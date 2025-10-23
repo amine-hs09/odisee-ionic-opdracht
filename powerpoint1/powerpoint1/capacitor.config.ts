01f029f0bbe685ff199eee0c8b7e1d25ede74915
amine-hs09 <mohamedamine.hssinoui@student.odisee.be>
2025-10-23 17:34:09 +0200

    oke

---
+++ powerpoint1/powerpoint1/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.powerpoint1',
+  appName: 'powerpoint1',
+  webDir: 'dist'
+};
+
+export default config;
