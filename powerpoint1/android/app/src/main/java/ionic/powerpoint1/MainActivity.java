dfd946d7e2cc6db89856059da87214fb193b03a3
amine-hs09 <mohamedamine.hssinoui@student.odisee.be>
2025-10-23 18:30:08 +0200

    oke

--- powerpoint1/android/app/src/main/java/ionic/powerpoint1/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package ionic.powerpoint1;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- powerpoint1/capacitor.config.js
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'ionic.powerpoint1',
-  appName: 'powerpoint1',
-  webDir: 'dist'
-};
-
-export default config;
