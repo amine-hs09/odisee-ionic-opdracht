a3d6f3d3bd25cb5e893f511f2b603cd35d47952a
amine-hs09 <mohamedamine.hssinoui@student.odisee.be>
2025-10-23 14:23:03 +0200

    oke

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.wn.app.test',
+  appName: 'ionic-wn-app-test',
+  webDir: 'dist'
+};
+
+export default config;
