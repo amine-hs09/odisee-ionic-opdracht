5866569ee2c55cabc63502beb55127e6cf6c3dc7
amine-hs09 <mohamedamine.hssinoui@student.odisee.be>
2025-10-27 09:49:45 +0100

    oke

--- powerpoint1/src/router/index.js
+++ powerpoint1/src/router/index.js
@@ -1,8 +1,7 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-// import { RouteRecordRaw } from 'vue-router'; // Ligne supprimée
 import TabsPage from '../views/TabsPage.vue'
 
-const routes = [ // ": Array<RouteRecordRaw>" supprimé ici
+const routes = [
   {
     path: '/',
     redirect: '/tabs/tab1'
