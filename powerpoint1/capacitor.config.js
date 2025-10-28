68d325724ab53f326d4cfbb748ffbaa472826f44
amine-hs09 <mohamedamine.hssinoui@student.odisee.be>
2025-10-28 11:57:18 +0100

    okok

--- powerpoint1/capacitor.config.js
+++ powerpoint1/capacitor.config.js
@@ -1,8 +1,8 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'ionic.powerpoint1',
-  appName: 'powerpoint1',
+// Pas d'import type ici
+// const config: CapacitorConfig = { ... } devient juste :
+const config = {
+  appId: 'ionic.powerpoint1', // Garde l'appId que tu avais
+  appName: 'powerpoint1',     // Garde l'appName que tu avais
   webDir: 'dist'
 };
 
