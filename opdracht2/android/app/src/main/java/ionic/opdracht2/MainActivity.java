8f9e14a75ee7401285fd9909eeae3d2a94e59011
amine-hs09 <mohamedamine.hssinoui@student.odisee.be>
2025-10-28 15:35:34 +0100

    dd

---
+++ opdracht2/android/app/src/main/java/ionic/opdracht2/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.opdracht2;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ opdracht2/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.opdracht2',
+  appName: 'opdracht2',
+  webDir: 'dist'
+};
+
+export default config;
