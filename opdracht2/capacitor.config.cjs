6e9fdfeeeb8f7eb2b49307081c687cfff1a57b94
amine-hs09 <mohamedamine.hssinoui@student.odisee.be>
2025-10-30 16:38:50 +0100

    aaa

---
+++ opdracht2/capacitor.config.cjs
@@ -0,0 +1,8 @@
+const config = {
+  appId: 'be.aminehs.concertapp',
+  appName: 'ConcertAppMohamedAmine',
+  webDir: 'dist'
+};
+
+// Utilisation ESM car package.json contient "type": "module"
+export default config;
--- opdracht2/capacitor.config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  appId: 'be.aminehs.concertapp', // Ton Bundle ID
-  appName: 'ConcertAppMohamedAmine', // Le nom de ton app
-  webDir: 'dist',
-};
-
-// Utilisation ESM car `package.json` contient "type": "module"
-export default config;
